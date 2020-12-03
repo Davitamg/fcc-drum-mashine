b13acbb062495c42c6556a9776a676e1b39b98b0
Davit Amaghlobeli <amaglobeli53@gmail.com>
2020-12-03 13:26:09 +0400

    Updates

---
+++ precache-manifest.3695a5be4f791f8c24b3549bbf03ef51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5123e8a30781b82fe61aaea947958bb",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "58a7621e058bc01e1a3a",
+    "url": "/my-app/static/css/main.8a11479d.chunk.css"
+  },
+  {
+    "revision": "1d411ee13cec0824c2a7",
+    "url": "/my-app/static/js/2.8626492a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.8626492a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58a7621e058bc01e1a3a",
+    "url": "/my-app/static/js/main.ba0b32e7.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  }
+]);
